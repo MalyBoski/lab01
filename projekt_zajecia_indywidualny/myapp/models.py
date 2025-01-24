b5a98e19717b06ac5c66f4e7bebf5addc48358e6
kacpermateo@gmail.com <kacpermateo@gmail.com>
2025-01-24 18:04:28 +0100

    zadania lab9

--- projekt_zajecia_indywidualny/myapp/models.py
+++ projekt_zajecia_indywidualny/myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import date
+from django.contrib.auth.models import User
 
 
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
@@ -26,7 +27,7 @@ class Osoba(models.Model):
     plec = models.IntegerField(choices=PLCIE.choices, default=PLCIE.choices[2][0])
     stanowisko = models.ForeignKey('Stanowisko', on_delete = models.CASCADE)
     data_dodania = models.DateField(default = date.today, blank=False, null=False)
-    
+    wlasciciel = models.ForeignKey(User, on_delete=models.CASCADE, blank = True, null = True)
     def __str__(self):
         return f'{self.imie} {self.nazwisko}' 
     
